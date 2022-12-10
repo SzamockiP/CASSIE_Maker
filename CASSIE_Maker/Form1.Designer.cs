32ce379e486acafb136eb652cf5e9f0a894f9ece
Piotr Szamocki <szamocki.piotr04@gmail.com>
2022-12-10 18:36:05 +0100

    Add project files.

---
+++ CASSIE_Maker/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace CASSIE_Maker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 89);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(85, 55);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(194, 142);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ CASSIE_Maker/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+using System.Threading;
+using NAudio;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System.Runtime.Remoting.Channels;
+
+
+namespace CASSIE_Maker
+{
+    public class Phoneme
+    {
+        public string name;
+        public SoundPlayer soundPlayer;
+        public string location;
+        public TimeSpan duration;
+
+        public Phoneme(string name, string location)
+        {
+            this.name = name;
+            this.location = location;
+
+            this.soundPlayer = new SoundPlayer();
+            soundPlayer.SoundLocation = this.location;
+
+            WaveFileReader waveFileReader = new WaveFileReader(location);
+            this.duration = waveFileReader.TotalTime;
+            waveFileReader.Dispose();
+        }
+
+        public void Play()
+        {
+            this.soundPlayer.Play();
+        }
+    }
+    public partial class Form1 : Form
+    {
+        Dictionary<string, Phoneme> phonemesSounds = new Dictionary<string, Phoneme>();
+        Dictionary<string, string[]> dictionary = new Dictionary<string, string[]>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            load_phonemes_sounds();
+
+            richTextBox1.Text = "The quick brown fox jumps over the lazy dog";
+
+            // Read file with all words
+            string[] dict = File.ReadAllText(@"C:\Users\Altin\OneDrive\Pulpit\cmu_dict.txt").Split('\n');
+
+            string word;
+            string[] phonemes;
+
+            // Add all words with pronunciations to dictionary
+            foreach (string line in dict)
+            {
+                // Try beacause of 5 the same keys of items
+                try
+                {
+                    // Get word and phonemes from line and add them
+                    word = line.Trim().Split(null, 2)[0].Trim();
+                    phonemes = line.Trim().Split(null, 2)[1].Split(null);
+                    phonemes = phonemes.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+                    dictionary.Add(word,phonemes);
+                }
+                catch (ArgumentException er)
+                {
+                    Console.WriteLine(er.Message);
+                }
+            }
+            
+                
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string text = richTextBox1.Text.Trim().Replace('\n',' ');
+            string[] meaningParts = text.Split(' ');
+            meaningParts = meaningParts.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            Task playSentence = Task.Factory.StartNew(() =>
+            {
+                foreach (string part in meaningParts)
+                {
+                    if (part.Trim().Length <= 0)
+                        continue;
+                    word_player(part.ToUpper());
+                    Thread.Sleep(100);
+                }
+            });
+            
+            
+        }
+
+
+        private void word_player(string word)
+        {
+            string[] phonemesNames;
+            if (!dictionary.TryGetValue(word, out phonemesNames))
+                return;
+
+            for (int i = 0; i < phonemesNames.Length; i++)
+            {
+                phonemesSounds[phonemesNames[i]].Play();
+                Thread.Sleep((int) Math.Round(phonemesSounds[phonemesNames[i]].duration.Milliseconds / 1.5));
+            } 
+        }
+
+        private void load_phonemes_sounds()
+        {
+            string[] phonemesNames = new string[]
+            {
+                "AA",
+                "AE",
+                "AH",
+                "AO",
+                "AW",
+                "AY",
+                "B",
+                "CH",
+                "D",
+                "DH",
+                "EH",
+                "ER",
+                "EY",
+                "F",
+                "G",
+                "HH",
+                "IH",
+                "IY",
+                "JH",
+                "K",
+                "L",
+                "M",
+                "N",
+                "NG",
+                "OW",
+                "OY",
+                "P",
+                "R",
+                "S",
+                "SH",
+                "T",
+                "TH",
+                "UH",
+                "UW",
+                "V",
+                "W",
+                "Y",
+                "Z",
+                "ZH"
+            };
+
+            foreach(string phonemeName in phonemesNames)
+            {
+                string location = @"D:\\!Downloads\\SCP_SL-20221206T161301Z-001\\Words_Numbers_Letters\\Phonemes_CMU\\" + phonemeName + ".wav";
+
+                phonemesSounds.Add(phonemeName, new Phoneme(phonemeName, location));
+            }
+        }
+    }
+}
